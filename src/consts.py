6698b6e07f9fb677f6af2167c16216f23e7a469b
Sayan Faraz <sayan.faraz@gmail.com>
2026-02-19 15:05:52 -0500

    event_id -> get_event_id()

---
+++ src/consts.py
@@ -0,0 +1,7 @@
+def get_event_id():
+    event_id = {'Sleep stage W': 0,
+                'Sleep stage 1': 1,
+                'Sleep stage 2': 2,
+                'Sleep stage 3/4': 3,
+                'Sleep stage R': 4}
+    return event_id
