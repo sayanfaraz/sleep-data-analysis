5efc7e941e22a2caa40857197d4cd34383256462
Sayan Faraz <sayan.faraz@gmail.com>
2026-03-29 01:26:49 -0400

    Experiments comparing multiple models

--- src/train.py
+++ src/train.py
@@ -2,7 +2,8 @@
 from src import data_objs, consts, preprocess
 
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score, roc_auc_score
 from sklearn.model_selection import StratifiedKFold, cross_val_score
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
@@ -23,11 +24,20 @@ import mlflow
 
 #     train
 
+models = {
+    "RandomForest": RandomForestClassifier(),
+    "SVM": SVC(probability=True)    #  change to False later, and proba -> decision_function
+}
+
+def apply_smote(X, y):
+    sm = SMOTE()
+    return sm.fit_resample(X, y)
+
 def train():
     RAND_STATE_INT = 10
     rng = np.random.default_rng()
 
-    mlflow.sklearn.autolog()
+    mlflow.set_experiment("EEG Classification")
 
     # Load Data
     print("Loading data ...")
@@ -45,36 +55,66 @@ def train():
 
     dataset_df = preprocess.bandpower_dict_to_df(sleep_stage_rel_bandpower, consts.get_event_ids())
 
-    # Hyperparameter search
-    
-
-    # Train
-    print("Train ...")
     X_train, X_test, y_train, y_test = train_test_split(dataset_df[list(dataset_df.columns)[:-1]],
-                                                  dataset_df[list(dataset_df.columns)[-1]],
-                                                  test_size=0.33,
-                                                  random_state=RAND_STATE_INT)
+                                                        dataset_df[list(dataset_df.columns)[-1]],
+                                                        test_size=0.33,
+                                                        random_state=RAND_STATE_INT)
+
+    for model_name, model in models.items():
+        mlflow.sklearn.autolog()
+
+        with mlflow.start_run(run_name=model_name):
+
+            # Hyperparameter search
+            
+
+            # Train
+            print("Train ...")
+            
+            # sm = SMOTE(random_state = RAND_STATE_INT)
+            # X_train_smote, y_train_smote = sm.fit_resample(X_train, y_train)
 
 
-    rf = RandomForestClassifier()
-    rf.fit(X_train, y_train)
+            # rf = RandomForestClassifier()
+            # rf.fit(X_train, y_train)
 
-    # Val
-    # y_pred = rf.predict(X_test)
+            rf = RandomForestClassifier()
+            rf.fit(X_train, y_train)
+            # rf.fit(X_train_smote, y_train_smote)
 
-    rf.score(X_test, y_test)
+            # Val
+            train_score = rf.score(X_train, y_train)
+            
+            y_pred = rf.predict(X_test)
+            y_prob = rf.predict_proba(X_test)[:, 1]  # for AUC
 
-    # cm = confusion_matrix(y_test, y_pred)
-    # accuracy = accuracy_score(y_test, y_pred)
-    # precision = precision_score(y_test, y_pred, average='weighted')
-    # recall = recall_score(y_test, y_pred, average='weighted')
-    # f1 = f1_score(y_test, y_pred, average='weighted')
+            mlflow.log_metrics({
+                'accuracy': accuracy_score(y_test, y_pred),
+                "f1": f1_score(y_test, y_pred, average='weighted'),
+                "precision": precision_score(y_test, y_pred, average='weighted'),
+                "recall": recall_score(y_test, y_pred, average='weighted')
+                # "auc": roc_auc_score(y_test, y_prob, multi_class='ovr', average='weighted'),
+            })
 
-    # Package models
+            
+
+            # cm = confusion_matrix(y_test, y_pred)
+            # accuracy = accuracy_score(y_test, y_pred)
+            # precision = precision_score(y_test, y_pred, average='weighted')
+            # recall = recall_score(y_test, y_pred, average='weighted')
+            # f1 = f1_score(y_test, y_pred, average='weighted')
+
+            # mlflow.log_metric("cm", cm)
+            # mlflow.log_metric("accuracy", accuracy)
+            # mlflow.log_metric("precision", precision)
+            # mlflow.log_metric("recall", recall)
+            # mlflow.log_metric("f1", f1)
+
+        # Package models
 
 
 if __name__ == "__main__":
-    main()
+    train()
 
 
 class SubjectDataManager():
