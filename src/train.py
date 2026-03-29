75b17e068546be3ba029e01a110e9f72652d7526
Sayan Faraz <sayan.faraz@gmail.com>
2026-03-29 02:47:35 -0400

    F1 weighted+macro

--- src/train.py
+++ src/train.py
@@ -83,15 +83,15 @@ def train():
 
             mlflow.log_metrics({
                 'accuracy': accuracy_score(y_test, y_pred),
-                "f1": f1_score(y_test, y_pred, average='weighted'),
+                "f1_weighted": f1_score(y_test, y_pred, average='weighted'),
+                "f1_macro": f1_score(y_test, y_pred, average='macro'),
                 "precision": precision_score(y_test, y_pred, average='weighted'),
                 "recall": recall_score(y_test, y_pred, average='weighted'),
                 "auc_weighted": roc_auc_score(y_test, y_prob, multi_class='ovo', average='weighted'),
-                "auc_macro": roc_auc_score(y_test, y_prob, multi_class='ovo', average='weighted')
+                "auc_macro": roc_auc_score(y_test, y_prob, multi_class='ovo', average='macro')
             })
 
             
-
             # cm = confusion_matrix(y_test, y_pred)
             # accuracy = accuracy_score(y_test, y_pred)
             # precision = precision_score(y_test, y_pred, average='weighted')
